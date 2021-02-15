2f2c43f3eeb0c680419164fc95e5ed402ededb76
Bernardo Busse <developer@busse.com.br>
2021-02-15 18:57:52 -0300

    commit final aula 5

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,18 @@
-import React from 'react';
 import Menu from '../src/components/commons/Menu';
+import Footer from '../src/components/commons/Footer';
 
 export default function Home()
 {
   return (
-    <Menu></Menu>
+    <div style={{
+      flex: '1',
+      display: 'flex',
+      flexWrap: 'wrap',
+      flexDirection: 'column',
+      justifyContent: 'space-between'
+    }}>
+      <Menu />
+      <Footer />
+    </div>
   );
 }
